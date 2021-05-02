96f0c9616fdf6aab75f6c1019fa283240c57f501
Kittipong Panchon <kittipong.panchon@gmail.com>
2021-05-03 01:27:13 +0700

    update

---
+++ libs/mailer/src/mailer.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MailerService } from '@my-guardian-api/mailer/mailer.service'
+
+@Module({
+  imports: [ConfigModule],
+  providers: [MailerService],
+  exports: [MailerService]
+})
+export class MailerModule {
+}
+
