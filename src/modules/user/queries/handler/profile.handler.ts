cbd689f1e5f5c93a88b8fc0517137ba69acfc0ed
Kittipong Kittipong <jiw.kittipong@bitkub.com>
2021-06-02 11:00:24 +0700

    udpate

--- src/modules/user/queries/handler/profile.handler.ts
+++ src/modules/user/queries/handler/profile.handler.ts
@@ -1,19 +1,25 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
 import { ProfileQuery } from '../query'
-import { CustomerModel, UserModel } from '@my-guardian-api/database'
+import { CustomerModel, ShopModel, UserModel } from '@my-guardian-api/database'
 import { EntityManager } from 'typeorm'
 import { RoleEnum } from '@my-guardian-api/common'
 
 @QueryHandler(ProfileQuery)
 export class ProfileHandler implements IQueryHandler<ProfileQuery> {
-  constructor(private readonly entityManager: EntityManager) {}
+  constructor(private readonly entityManager: EntityManager) {
+  }
 
   async execute({ user }: ProfileQuery): Promise<UserModel> {
     let profile: any = {}
     switch (user?.role?.key) {
       case RoleEnum.CUSTOMER:
         profile = await this.entityManager.findOne(CustomerModel, {
-          userId: user.id,
+          userId: user.id
+        })
+        break
+      case RoleEnum.SHOP:
+        profile = await this.entityManager.findOne(ShopModel, {
+          userId: user.id
         })
         break
     }
@@ -22,6 +28,13 @@ export class ProfileHandler implements IQueryHandler<ProfileQuery> {
 
     delete user.password
     delete user.salt
+
+    if (user?.role?.key === RoleEnum.SHOP) {
+      delete user['profile']?.schedules
+      delete user['profile']?.prices
+      delete user['profile'].bookings
+
+    }
 
     return user
   }
