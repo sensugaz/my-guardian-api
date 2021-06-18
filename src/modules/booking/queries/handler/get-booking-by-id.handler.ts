d12508a84edb1dab78489bec51553f28e64db826
Kittisan Dechtheerapong <laonapaporn_22@hotmail.com>
2021-06-19 03:12:52 +0700

    fix getbooking by id endpoint

--- src/modules/booking/queries/handler/get-booking-by-id.handler.ts
+++ src/modules/booking/queries/handler/get-booking-by-id.handler.ts
@@ -1,20 +1,27 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
 import { GetBookingByIdQuery } from '../query'
 import { InjectRepository } from '@nestjs/typeorm'
-import { BookingRepository, CustomerRepository, ShopRepository } from '@my-guardian-api/database/repositories'
+import {
+  BookingRepository,
+  CustomerRepository,
+  ShopRepository,
+} from '@my-guardian-api/database/repositories'
 import { BookingModel } from '@my-guardian-api/database'
 import { ApiException } from '@my-guardian-api/common'
 import { HttpStatus } from '@nestjs/common'
 
 @QueryHandler(GetBookingByIdQuery)
-export class GetBookingByIdHandler implements IQueryHandler<GetBookingByIdQuery> {
-  constructor(@InjectRepository(BookingRepository)
-              private readonly bookingRepository: BookingRepository,
-              @InjectRepository(CustomerRepository)
-              private readonly customerRepository: CustomerRepository,
-              @InjectRepository(ShopRepository)
-              private readonly shopRepository: ShopRepository) {
-  }
+export class GetBookingByIdHandler
+  implements IQueryHandler<GetBookingByIdQuery>
+{
+  constructor(
+    @InjectRepository(BookingRepository)
+    private readonly bookingRepository: BookingRepository,
+    @InjectRepository(CustomerRepository)
+    private readonly customerRepository: CustomerRepository,
+    @InjectRepository(ShopRepository)
+    private readonly shopRepository: ShopRepository,
+  ) {}
 
   async execute(query: GetBookingByIdQuery): Promise<BookingModel> {
     const tableName = 'booking'
@@ -28,12 +35,21 @@ export class GetBookingByIdHandler implements IQueryHandler<GetBookingByIdQuery>
 
     switch (query.user.role.key) {
       case 'CUSTOMER':
-        const customer = await this.customerRepository.findOne({ userId: query.user.id })
-        queryBuilder = queryBuilder.andWhere('booking.customer.id = :customerId', { customerId: customer.id })
+        const customer = await this.customerRepository.findOne({
+          userId: query.user.id,
+        })
+        queryBuilder = queryBuilder.andWhere(
+          'booking.customer.id = :customerId',
+          { customerId: customer.id },
+        )
         break
       case 'SHOP':
-        const shop = await this.customerRepository.findOne({ userId: query.user.id })
-        queryBuilder = queryBuilder.andWhere('booking.shop.id = :shopId', { shopId: shop.id })
+        const shop = await this.shopRepository.findOne({
+          userId: query.user.id,
+        })
+        queryBuilder = queryBuilder.andWhere('booking.shop.id = :shopId', {
+          shopId: shop.id,
+        })
         break
     }
 
@@ -44,11 +60,10 @@ export class GetBookingByIdHandler implements IQueryHandler<GetBookingByIdQuery>
         type: 'application',
         module: 'booking',
         codes: ['booking_not_found'],
-        statusCode: HttpStatus.BAD_REQUEST
+        statusCode: HttpStatus.BAD_REQUEST,
       })
     }
 
     return booking
   }
-
 }
