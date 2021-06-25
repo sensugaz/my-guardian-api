71175f4786a58c0d67127aa846caeb1da58a31e5
Kittipong Kittipong <jiw.kittipong@bitkub.com>
2021-06-25 20:49:39 +0700

    update

--- libs/mailer/src/mailer.service.ts
+++ libs/mailer/src/mailer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import * as Mail from 'nodemailer/lib/mailer'
 import * as hbs from 'nodemailer-express-handlebars'
 import { ConfigService } from '@nestjs/config'
@@ -17,50 +17,58 @@ export class MailerService {
       secure: true,
       auth: {
         user: configService.get<string>('SMTP_USER'),
-        pass: configService.get<string>('SMTP_PASS'),
-      },
+        pass: configService.get<string>('SMTP_PASS')
+      }
     })
   }
 
   send(to: string, subject: string, content: string): Promise<SentMessageInfo> {
-    const mailOptions = {
-      from: this.configService.get<string>('SMTP_USER'),
-      to,
-      subject,
-      html: content,
-    }
+    try {
+      const mailOptions = {
+        from: this.configService.get<string>('SMTP_USER'),
+        to,
+        subject,
+        html: content
+      }
 
-    return this.transporter.sendMail(mailOptions)
+      return this.transporter.sendMail(mailOptions)
+    } catch (e) {
+      Logger.error(e)
+    }
   }
 
   sendWithTemplate(
     to: string,
     subject: string,
     context: any,
-    template: string,
+    template: string
   ): Promise<SentMessageInfo> {
-    this.transporter.use(
-      'compile',
-      hbs({
-        viewEngine: {
-          extName: '.hbs',
-          partialsDir: join(__dirname, 'templates'),
-          layoutsDir: join(__dirname, 'templates'),
-          defaultLayout: '',
-        },
-        viewPath: join(__dirname, 'templates'),
-        extName: '.hbs',
-      }),
-    )
+    try {
+      this.transporter.use(
+        'compile',
+        hbs({
+          viewEngine: {
+            extName: '.hbs',
+            partialsDir: join(__dirname, 'templates'),
+            layoutsDir: join(__dirname, 'templates'),
+            defaultLayout: ''
+          },
+          viewPath: join(__dirname, 'templates'),
+          extName: '.hbs'
+        })
+      )
 
-    const mailOptions = {
-      from: this.configService.get<string>('SMTP_USER'),
-      to,
-      subject,
-      context: { context },
-      template,
+      const mailOptions = {
+        from: this.configService.get<string>('SMTP_USER'),
+        to,
+        subject,
+        context: { context },
+        template
+      }
+
+      return this.transporter.sendMail(mailOptions)
+    } catch (e) {
+      Logger.error(e)
     }
-
-    return this.transporter.sendMail(mailOptions)
   }
 }
