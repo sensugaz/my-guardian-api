d23e1eadfcd02be18d33debad3d7782b7d56f4d7
Kittipong Kittipong <jiw.kittipong@bitkub.com>
2021-07-22 18:25:34 +0700

    upload

--- libs/database/src/models/base.model.ts
+++ libs/database/src/models/base.model.ts
@@ -1,29 +1,24 @@
-import {
-  CreateDateColumn,
-  DeleteDateColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm'
+import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
 
 export class BaseModel {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
   @CreateDateColumn({
-    type: 'timestamp without time zone',
-    name: 'created_date',
+    type: 'timestamp with time zone',
+    name: 'created_date'
   })
   createdDate: Date
 
   @UpdateDateColumn({
-    type: 'timestamp without time zone',
-    name: 'updated_date',
+    type: 'timestamp with time zone',
+    name: 'updated_date'
   })
   updatedDate: Date
 
   @DeleteDateColumn({
-    type: 'timestamp without time zone',
-    name: 'deleted_date',
+    type: 'timestamp with time zone',
+    name: 'deleted_date'
   })
   deletedDate: Date
 }
