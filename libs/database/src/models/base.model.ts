43b83c75699499bb3bf29ca026022ef45702afe1
Kittisan Dechtheerapong <laonapaporn_22@hotmail.com>
2021-07-26 20:37:22 +0700

    remove transformer

--- libs/database/src/models/base.model.ts
+++ libs/database/src/models/base.model.ts
@@ -13,52 +13,18 @@ export class BaseModel {
   @CreateDateColumn({
     type: 'timestamp with time zone',
     name: 'created_date',
-    transformer: {
-      to(value: Date): any {
-        if (!value) {
-          return moment().add(2, 'hours').toDate()
-        }
-        return moment(value).add(2, 'hours').toDate()
-      },
-      from(value: Date): any {
-        return value
-      },
-    },
   })
   createdDate: string
 
   @UpdateDateColumn({
     type: 'timestamp with time zone',
     name: 'updated_date',
-    transformer: {
-      to(value: Date): any {
-        if (!value) {
-          return moment().add(2, 'hours').toDate()
-        }
-        return moment(value).add(2, 'hours').toDate()
-      },
-      from(value: Date): any {
-        return value
-      },
-    },
   })
   updatedDate: Date
 
   @DeleteDateColumn({
     type: 'timestamp',
     name: 'deleted_date',
-    transformer: {
-      to(value: Date): any {
-        if (value) {
-          return moment().add(2, 'hours').toDate()
-        }
-
-        return value
-      },
-      from(value: Date): any {
-        return value
-      },
-    },
   })
   deletedDate: Date
 }
