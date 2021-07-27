dba8c2d15cfab566665310ee44f2916e58a85a94
Kittipong Kittipong <jiw.kittipong@bitkub.com>
2021-07-28 02:10:51 +0700

    upload

--- src/modules/webhook/commands/handler/webhook.handler.ts
+++ src/modules/webhook/commands/handler/webhook.handler.ts
@@ -4,14 +4,10 @@ import {
   BookingRepository,
   ShopRepository,
   UserRepository,
-  VoucherHistoryRepository,
+  VoucherHistoryRepository
 } from '@my-guardian-api/database/repositories'
 import { InjectRepository } from '@nestjs/typeorm'
-import {
-  ApiException,
-  BookingStatusEnum,
-  PaymentStatusEnum,
-} from '@my-guardian-api/common'
+import { ApiException, BookingStatusEnum, PaymentStatusEnum } from '@my-guardian-api/common'
 import { HttpStatus, Logger } from '@nestjs/common'
 import { BookingModel } from '@my-guardian-api/database'
 import { MailerService } from '@my-guardian-api/mailer'
@@ -29,17 +25,18 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
     private readonly mailerService: MailerService,
-    private readonly configService: ConfigService,
-  ) {}
+    private readonly configService: ConfigService
+  ) {
+  }
 
   async execute({ body }: WebhookCommand): Promise<BookingModel> {
     const booking = await this.bookingRepository.findOne(
       {
-        id: body.data?.object?.metadata?.bookingId,
+        id: body.data?.object?.metadata?.bookingId
       },
       {
-        relations: ['shop', 'customer'],
-      },
+        relations: ['shop', 'customer']
+      }
     )
 
     if (!booking) {
@@ -47,7 +44,7 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
         type: 'application',
         module: 'booking',
         codes: ['booking_not_found'],
-        statusCode: HttpStatus.BAD_REQUEST,
+        statusCode: HttpStatus.BAD_REQUEST
       })
     }
 
@@ -64,15 +61,15 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
           await this.voucherHistoryRepository.save(
             this.voucherHistoryRepository.create({
               user: {
-                id: booking.customer.userId,
+                id: booking.customer.userId
               },
-              code: booking.voucherCode,
-            }),
+              code: booking.voucherCode
+            })
           )
         }
 
         const user = await this.userRepository.findOne({
-          id: booking.shop.userId,
+          id: booking.shop.userId
         })
 
         if (booking.qty == 1) {
@@ -84,9 +81,9 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
                 url: `${this.configService.get('BACKOFFICE_URL')}/order/${
                   booking.id
                 }/detail`,
-                bookingId: booking.id,
+                bookingId: booking.id
               },
-              'solo-booking',
+              'solo-booking'
             )
           } catch (e) {
             Logger.error(e)
@@ -100,9 +97,9 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
                 url: `${this.configService.get('BACKOFFICE_URL')}/order/${
                   booking.id
                 }/detail`,
-                bookingId: booking.id,
+                bookingId: booking.id
               },
-              'duo-booking',
+              'duo-booking'
             )
           } catch (e) {
             Logger.error(e)
@@ -121,10 +118,11 @@ export class WebhookHandler implements ICommandHandler<WebhookCommand> {
         booking.updateBookingStatus(BookingStatusEnum.FAILED)
         break
       case 'charge.refunded':
+        booking.updateBookingStatus(BookingStatusEnum.CANCELLED)
         booking.updatePaymentStatus(PaymentStatusEnum.REFUND)
         break
     }
-    console.log(`booking`, booking)
+    
     return await this.bookingRepository.save(booking)
   }
 }
